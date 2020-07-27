f9933ee53c345ae522c5cdb43656353ba4ffe787
Leszek <leszek@mamami.digital>
2020-07-27 15:14:01 +0200

    showcase add demo

--- projects/rfx-logger-showcase/src/app/app.component.ts
+++ projects/rfx-logger-showcase/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RfxLoggerService } from '../../../rfx-logger/src/lib/rfx-logger.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'rfx-logger-showcase';
+
+  public customMessage: string;
+  public debugData: Array<string>;
+  public isDebugDataDisabled: boolean;
+
+  constructor(private rfxLoggerService: RfxLoggerService) {
+    this.debugData = ['this', 'is', 'a', 'nice', 'test'];
+  }
+
+  private getCustomMessage(): string {
+    return this.customMessage ? this.customMessage : 'your message';
+  }
+
+  private getDebugData(): Array<string> {
+    return this.isDebugDataDisabled ? undefined : this.debugData;
+  }
+
+  public toggleDebugData(): void {
+    this.isDebugDataDisabled = !this.isDebugDataDisabled;
+  }
+
+  public rfxLoggerSuccess(): void {
+    this.rfxLoggerService.success(this.getCustomMessage(), this.getDebugData());
+  }
+
+  public rfxLoggerWarning(): void {
+    this.rfxLoggerService.warning(this.getCustomMessage(), this.getDebugData());
+  }
+
+  public rfxLoggerError(): void {
+    this.rfxLoggerService.error(this.getCustomMessage(), this.getDebugData());
+  }
+
 }
