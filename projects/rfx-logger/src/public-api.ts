d9d78cd01fbe866ec9442b8ab750a4a9d7e22b9e
RedFoxxo <blurio90@gmail.com>
2020-09-17 18:17:39 +0200

    fix wrong rfx-logger.interceptor import in public-api

--- projects/rfx-logger/src/public-api.ts
+++ projects/rfx-logger/src/public-api.ts
@@ -3,6 +3,6 @@
  */
 
 export * from './lib/configuration.model';
-export * from './lib/rfx-http-logger.interceptor';
+export * from './lib/rfx-logger.interceptor';
 export * from './lib/rfx-logger.service';
 export * from './lib/rfx-logger.module';
