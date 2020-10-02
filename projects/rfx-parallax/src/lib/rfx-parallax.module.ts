5da30410227a7636e6538d5fbc92351d46145385
RedFoxxo <blurio90@gmail.com>
2020-10-02 15:30:55 +0200

    correctly export directive + update package version + minor fixes

--- projects/rfx-parallax/src/lib/rfx-parallax.module.ts
+++ projects/rfx-parallax/src/lib/rfx-parallax.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RfxParallaxDirective } from './rfx-parallax.directive';
 
 @NgModule({
-  declarations: [RfxParallaxDirective],
-  imports: [
+  declarations: [
+    RfxParallaxDirective
   ],
-  exports: []
+  imports: [],
+  exports: [
+    RfxParallaxDirective
+  ]
 })
 export class RfxParallaxModule { }
