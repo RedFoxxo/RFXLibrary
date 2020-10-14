3285197e265b019ac08db7d46b3600eae717c952
RedFoxxo <blurio90@gmail.com>
2020-10-14 08:03:25 +0200

    rfx-parallax demo test

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RfxParallaxService } from 'rfx-parallax';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private rfxParallaxService: RfxParallaxService
+  ) {}
 
+  public ngOnInit(): void {
+    this.rfxParallaxService.initListeners();
+  }
 }
