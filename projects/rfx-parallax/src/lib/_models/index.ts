cc6b021c7c17da96f7227cf03cb19ba472fa1313
RedFoxxo <blurio90@gmail.com>
2020-11-08 17:10:45 +0100

    fix wrong directive initialization

---
+++ projects/rfx-parallax/src/lib/_models/index.ts
@@ -0,0 +1,2 @@
+export * from './rfx-parallax-boundaries.model';
+export * from './rfx-parallax-position.model';
--- projects/rfx-parallax/src/lib/index.ts
+++ projects/rfx-parallax/src/lib/index.ts
@@ -1,4 +1,4 @@
+export * from './_models';
 export * from './rfx-parallax.service';
 export * from './rfx-parallax.module';
 export * from './rfx-parallax.directive';
-export * from './rfx-parallax-boundaries.model';
