da3c273aa6a8cc8fea85cbf2f77ef3e5dd075ed2
RedFoxxo <blurio90@gmail.com>
2020-11-09 11:07:50 +0100

    fix rfx-scroll-animation directive not included error

---
+++ projects/rfx-scroll-animation/src/lib/_models/index.ts
@@ -0,0 +1,2 @@
+export * from './render-style.model';
+export * from './animation-type.enum';
--- projects/rfx-scroll-animation/src/lib/index.ts
+++ projects/rfx-scroll-animation/src/lib/index.ts
@@ -1,4 +1,4 @@
 export * from './rfx-scroll-animation.service';
 export * from './rfx-scroll-animation.module';
-export * from './animation-type.enum';
-export * from './render-style.model';
+export * from './rfx-scroll-animation.directive';
+export * from './_models';
