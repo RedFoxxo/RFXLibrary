cb4b8684bb20647f21eb4007686cab52b97a35c0
RedFoxxo <blurio90@gmail.com>
2020-12-23 12:45:06 +0100

    fix rfx-logger lib import and update global version

--- projects/rfx-logger/src/public-api.ts
+++ projects/rfx-logger/src/public-api.ts
@@ -2,4 +2,7 @@
  * Public API Surface of rfx-logger
  */
 
-export * from './lib';
+export * from './lib/configuration.model';
+export * from './lib/rfx-logger.module';
+export * from './lib/rfx-logger.service';
+export * from './lib/rfx-logger.interceptor';
