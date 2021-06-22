edd64f4ef2bf05cb737035ef2bb2e68803f31b5f
RedFoxxo <blurio90@gmail.com>
2021-06-22 12:32:58 +0200

    fix export and remove unused test

--- projects/rfx-logger/src/public-api.ts
+++ projects/rfx-logger/src/public-api.ts
@@ -2,7 +2,7 @@
  * Public API Surface of rfx-logger
  */
 
-export * from './lib/configuration.model';
+export * from './lib/models/configuration.model';
 export * from './lib/rfx-logger.module';
 export * from './lib/rfx-logger.service';
 export * from './lib/rfx-logger.interceptor';
