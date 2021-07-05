ab86c654df77bbd80d837e6c18aa449e534c528b
RedFoxxo <blurio90@gmail.com>
2021-07-05 17:28:06 +0200

    remove unused test files

--- projects/rfx-parallax/src/lib/rfx-parallax.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RfxParallaxDirective } from './rfx-parallax.directive';
-
-describe('RfxParallaxDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new RfxParallaxDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
