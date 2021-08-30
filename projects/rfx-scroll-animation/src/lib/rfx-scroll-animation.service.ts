bef059ac663831b718e54b1ed038735fbbcf66bc
RedFoxxo <blurio90@gmail.com>
2021-08-30 12:10:04 +0200

    add height change detection; add documentation

--- projects/rfx-scroll-animation/src/lib/rfx-scroll-animation.service.ts
+++ projects/rfx-scroll-animation/src/lib/rfx-scroll-animation.service.ts
@@ -1,47 +1,111 @@
-import { Injectable, OnDestroy, Renderer2, RendererFactory2 } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RfxScrollAnimationService implements OnDestroy {
-  private subjectScroll: Subject<undefined>;
+  /**
+   * Subscribe to body scroll changes.
+   * @type {Observable<number>}
+   */
+  private subjectScroll: BehaviorSubject<number>;
+
+  /**
+   * Subscribe to router navigation changes.
+   * @type {Observable<boolean>}
+   */
   private subjectNavigation: BehaviorSubject<boolean>;
 
-  private renderer: Renderer2;
-  private mouseScrollEvent!: () => void;
-  private routerNavEvent!: Subscription;
+  /**
+   * Subscribe to body height changes.
+   * @type {BehaviorSubject<number>}
+   */
+  private subjectHeight: BehaviorSubject<number>;
+
+  /**
+   * Current body element in use.
+   * @type {HTMLElement}
+   */
+  private bodyElement: HTMLElement | undefined;
+
+  /**
+   * Mouse scroll listener.
+   * @type {EventListener}
+   */
+  private mouseScrollEvent: EventListenerOrEventListenerObject;
+
+  /**
+   * Body height changes listener.
+   * @param {number} height
+   */
+  private bodyHeightEvent: ResizeObserver | undefined;
+
+  /**
+   * Router navigation event listener.
+   * @type {Subscription}
+   */
+  private routerNavEvent: Subscription | undefined;
 
   constructor(
-    private rendererFactory: RendererFactory2,
     private router: Router
   ) {
-    this.subjectScroll = new Subject<undefined>();
+    this.subjectScroll = new BehaviorSubject<number>(0);
+    this.subjectHeight = new BehaviorSubject<number>(0);
     this.subjectNavigation = new BehaviorSubject<boolean>(false);
-    this.renderer = this.rendererFactory.createRenderer(null, null);
+    this.mouseScrollEvent = this.onMouseScrollEvent.bind(this);
   }
 
   public ngOnDestroy(): void {
     this.destroyListeners();
   }
 
+  /**
+   * Destroy all listeners.
+   * @return {void}
+   */
   private destroyListeners(): void {
-    if (this.mouseScrollEvent) {
-      this.mouseScrollEvent();
-    }
-
+    window.removeEventListener('scroll', this.mouseScrollEvent, false);
+    this.bodyHeightEvent?.disconnect();
     this.routerNavEvent?.unsubscribe();
   }
 
-  public initListeners(scrollElement?: HTMLElement): void {
+  /**
+   * Destroy all extisting listeners, update body element and then create new listeners.
+   * Initialize mouse scroll, body height and router navigation listeners.
+   * Default body element is the document body element.
+   * @param {HTMLElement} bodyElement - Body element to use.
+   * @return {void}
+   */
+  public initListeners(bodyElement: HTMLElement = document.body): void {
     this.destroyListeners();
-    this.mouseScrollEvent = this.renderer.listen(scrollElement ?? document, 'scroll', () => this.onMouseScroll());
-    this.initRouterEventListener();
+    this.bodyElement = bodyElement;
+    this.bodyElement.addEventListener('scroll', this.mouseScrollEvent, false);
+    this.bodyHeightEvent = this.getBodyHeightEventListener(this.bodyElement);
+    this.routerNavEvent = this.getRouterEventListener();
   }
 
-  private initRouterEventListener(): void {
-    this.routerNavEvent = this.router.events.subscribe((event) => {
+  /**
+   * Initialize and return body element height changes listener.
+   * @param {HTMLElement} bodyElement - Body element to use.
+   * @return {ResizeObserver}
+   */
+  private getBodyHeightEventListener(bodyElement: HTMLElement): ResizeObserver {
+    const bodyHeightEventListener = new ResizeObserver((entries: ResizeObserverEntry[]) => {;
+      const height = entries[0].contentRect.height;
+      this.onHeightChangeEvent(height);
+    });
+    bodyHeightEventListener.observe(bodyElement);
+    return bodyHeightEventListener;
+  }
+
+  /**
+   * Initialize and return router navigation event listener.
+   * @return {Subscription}
+   */
+  private getRouterEventListener(): Subscription {
+    return this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.onRouterEvent(true);
       }
@@ -51,22 +115,52 @@ export class RfxScrollAnimationService implements OnDestroy {
     });
   }
 
-  private onRouterEvent(pageReady: boolean): void {
-    this.subjectNavigation.next(pageReady);
+  /**
+   * Trigger router navigation event.
+   * @param {boolean} ready - Is page ready to scroll.
+   * @return {void}
+   */
+  private onRouterEvent(ready: boolean): void {
+    this.subjectNavigation.next(ready);
   }
 
-  private onMouseScroll(): void {
-    this.subjectScroll.next();
+  /**
+   * Trigger mouse scroll event.
+   * @param {number} event - Mouse scroll event.
+   * @return {void}
+   */
+  private onMouseScrollEvent(event: Event): void {
+    this.subjectScroll.next((event.target as HTMLElement).scrollTop);
   }
 
-  public getMouseScroll(): Observable<undefined> {
+  /**
+   * Trigger body height event.
+   * @param {number} height - Body height.
+   */
+  private onHeightChangeEvent(height: number): void {
+    this.subjectHeight.next(height);
+  }
+
+  /**
+   * Get body scroll event.
+   * @return {Observable<number>}
+   */
+  public getMouseScroll(): Observable<number> {
     return this.subjectScroll.asObservable();
   }
 
+  /**
+   * Get body height event.
+   * @return {Observable<number>}
+   */
   public getRouterEvent(): Observable<boolean> {
     return this.subjectNavigation.asObservable();
   }
 
+  /**
+   * Get router navigation event.
+   * @return {Observable<number>}
+   */
   public getRouterEventValue(): boolean {
     return this.subjectNavigation.value;
   }
