49078bf5101afea7a58a08e83944a2b0660a5495
RedFoxxo <blurio90@gmail.com>
2021-08-31 18:23:09 +0200

    expose animation visibility enum for setVisibility method

--- projects/rfx-scroll-animation/src/public-api.ts
+++ projects/rfx-scroll-animation/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/rfx-scroll-animation.module';
 export * from './lib/rfx-scroll-animation.service';
 export * from './lib/rfx-scroll-animation/rfx-scroll-animation.component';
 export * from './lib/models/animation-type.enum';
+export * from './lib/models/animation-visiblity.enum';
