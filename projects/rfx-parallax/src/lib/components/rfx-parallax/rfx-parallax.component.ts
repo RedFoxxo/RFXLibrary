05b205bec6107270e8369a0d64e43afa032e0f59
Foxxo <blurio90@gmail.com>
2021-11-22 12:53:45 +0100

    solid parallax add user content support

--- projects/rfx-parallax/src/lib/components/rfx-parallax/rfx-parallax.component.ts
+++ projects/rfx-parallax/src/lib/components/rfx-parallax/rfx-parallax.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { RegistryService } from '../../services';
 
 @Component({
   selector: '[libRfxParallax]',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rfx-parallax.component.less']
 })
 export class RfxParallaxComponent implements OnInit {
+  /**
+   * Parallax element speed.
+   * e.g. 0 = no parallax, 1 = same speed as page
+   * Default is 0.5.
+   * @type {number}
+   */
+  @Input()
+  public parallaxSpeed: number;
 
-  constructor() { }
+  /**
+   * Disable parallax effect and restore default
+   * element position.
+   * Useful on low performance devices.
+   * @type {boolean}
+   */
+  @Input()
+  public isDisabled: boolean;
 
-  ngOnInit(): void {
+  /**
+   * Registered element index inside service.
+   * @type {number}
+   */
+  private elementIndex: number;
+
+  constructor(
+    private htmlElement: ElementRef,
+    private registryService: RegistryService
+  ) {
+    this.parallaxSpeed = 0.5;
+    this.isDisabled = false;
+    this.elementIndex = this.registryService.registerSolidElement(this);
   }
 
+  public ngOnInit(): void {
+    // TODO CALC
+    this.registryService.setSolidElementReady(this.elementIndex);
+  }
 }
+
